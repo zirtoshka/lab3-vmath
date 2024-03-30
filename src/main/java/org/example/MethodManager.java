d9f06ca07c05f4b9b387e0376159220b2ff2bda1
zirtoshka <367222@edu.itmo.ru>
2024-03-30 23:23:44 +0300

    basic functional

--- src/main/java/org/example/MethodManager.java
+++ src/main/java/org/example/MethodManager.java
@@ -39,6 +39,7 @@ public class MethodManager {
             res1 = res2;
             res2 = methodMap.get(methodNumber).apply(function);
         }
+        System.out.println("Значение интеграла: " + res2);
         System.out.println("Количество разбиений для достижения точности: " + numberOfSplits);
         numberOfSplits = n;
     }
@@ -57,25 +58,66 @@ public class MethodManager {
     }
 
     private BigDecimal leftRectangleMethod(Function<BigDecimal, BigDecimal> function) {
-        System.out.println("Это левые треугольники");
-        return BigDecimal.ONE;
+        BigDecimal res = BigDecimal.ZERO;
+        BigDecimal h = rightBorder.subtract(leftBorder).divide(BigDecimal.valueOf(numberOfSplits), MathContext.DECIMAL32);
+        BigDecimal x = leftBorder;
+        for (int i = 1; i <= numberOfSplits; i++) {
+            res = res.add(function.apply(x));
+            x = x.add(h);
+
+        }
+        res = res.multiply(h);
+        System.out.println("Это левые треугольники " + res);
+        return res;
     }
 
     private BigDecimal mediumRectangleMethod(Function<BigDecimal, BigDecimal> function) {
-        System.out.println("Это средние треугольники");
-        return BigDecimal.ONE;
+        BigDecimal res = BigDecimal.ZERO;
+        BigDecimal h = rightBorder.subtract(leftBorder).divide(BigDecimal.valueOf(numberOfSplits), MathContext.DECIMAL32);
+        BigDecimal hHalf = h.divide(BigDecimal.valueOf(2), MathContext.DECIMAL32);
+        BigDecimal x = leftBorder.add(hHalf);
+        for (int i = 1; i <= numberOfSplits; i++) {
+            res = res.add(function.apply(x));
+            x = x.add(h);
+        }
+        res = res.multiply(h);
+        System.out.println("Это средние треугольники " + res);
+        return res;
     }
 
 
     private BigDecimal trapezoidMethod(Function<BigDecimal, BigDecimal> function) {
-        System.out.println("Это трапеции");
-        return BigDecimal.ONE;
+        BigDecimal res = function.apply(leftBorder).add(function.apply(rightBorder)).divide(BigDecimal.valueOf(2), MathContext.DECIMAL32);
+        BigDecimal h = rightBorder.subtract(leftBorder).divide(BigDecimal.valueOf(numberOfSplits), MathContext.DECIMAL32);
+        BigDecimal x = leftBorder;
+        for (int i = 1; i < numberOfSplits; i++) {
+            x = x.add(h);
+            res = res.add(function.apply(x));
+        }
+        res = res.multiply(h);
+
+        System.out.println("Это трапеции " + res);
+        return res;
     }
 
     private BigDecimal simpsonMethod(Function<BigDecimal, BigDecimal> function) {
-        System.out.println("Это Симпсон");
+        BigDecimal res = function.apply(leftBorder).add(function.apply(rightBorder));
+        BigDecimal h = rightBorder.subtract(leftBorder).divide(BigDecimal.valueOf(numberOfSplits), MathContext.DECIMAL32);
+        BigDecimal x = leftBorder;
+        for (int i = 1; i < numberOfSplits; i++) {
+            x = x.add(h);
+            if (i % 2 == 0) {
+                res = res.add(function.apply(x).multiply(BigDecimal.valueOf(2)));
+            } else {
+                res = res.add(function.apply(x).multiply(BigDecimal.valueOf(4)));
+            }
 
-        return BigDecimal.ONE;
+        }
+        res = res.multiply(h.divide(BigDecimal.valueOf(3), MathContext.DECIMAL32));
+
+        System.out.println("Это Симпсон " + res);
+
+        return res;
     }
 
     public void setLeftBorder(BigDecimal leftBorder) {
